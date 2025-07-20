42b6f6b6def9d8876bf708624dea13bef0894d42
Nishant <nishant.dahiya2000@gmail.com>
2025-07-21 00:22:56 +0530

    backend: pass the schema to better auth
    we need to explicitely pass the schema to drizzle adapter's constructor for better-auth to work despite providing path in drizzle config

--- backend/src/utils/auth.ts
+++ backend/src/utils/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { db } from '../db/drizzle.js';
+import { schema } from '../db/schema.js';
 
 export const auth = betterAuth({
     trustedOrigins: ['http://localhost:3000'],
@@ -8,6 +9,7 @@ export const auth = betterAuth({
         enabled: true
     },
     database: drizzleAdapter(db, {
-        provider: 'pg'
+        provider: 'pg',
+        schema: schema
     })
 });

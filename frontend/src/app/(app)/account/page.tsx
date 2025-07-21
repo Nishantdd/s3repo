d487b5937aa60074283d856ed232d86a23d76a6f
Nishant <nishant.dahiya2000@gmail.com>
2025-07-21 23:04:59 +0530

    frontend: add a logout button to accounts page

--- frontend/src/app/(app)/account/page.tsx
+++ frontend/src/app/(app)/account/page.tsx
@@ -1,14 +1,26 @@
-"use client";
+'use client';
 
-import { authClient } from "@/lib/auth-client";
-import { Loading } from "@/components/Loading";
-import { redirect } from "next/navigation";
+import { authClient } from '@/lib/auth-client';
+import { Loading } from '@/components/Loading';
+import { redirect } from 'next/navigation';
+import { Button } from '@/components/ui/button';
 
 export default function Page() {
     const { data: session, isPending, error } = authClient.useSession();
 
     if (isPending) return <Loading />;
     if (!session || error) return redirect('/login');
-    
-    return (<div>Accounts page</div>)
+
+    const handleLogout = async (e: React.FormEvent) => {
+        e.preventDefault();
+        await authClient.signOut({
+            fetchOptions: {
+                onSuccess: () => {
+                    redirect('/login');
+                }
+            }
+        });
+    };
+
+    return <Button onClick={handleLogout}>Logout</Button>;
 }

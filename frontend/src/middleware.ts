6d471fb3a5454619e972fb0841c556a242bf97ed
Nishant <nishant.dahiya2000@gmail.com>
2025-07-21 22:58:53 +0530

    frontend: update middleware to only handle different redirect based on sessionCookie

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -2,13 +2,24 @@ import { NextResponse, NextRequest } from 'next/server';
 import { getSessionCookie } from 'better-auth/cookies';
 
 export function middleware(request: NextRequest) {
-    if (!getSessionCookie(request)) {
-        return NextResponse.redirect(new URL('/login', request.url));
+    const session = getSessionCookie(request);
+    const { pathname } = request.nextUrl;
+
+    const isAuthPage = pathname === '/login' || pathname === '/signup';
+
+    if (session) {
+        if (isAuthPage)
+            return NextResponse.redirect(new URL('/dashboard', request.url));
+        else
+            return NextResponse.next();
     } else {
-        return NextResponse.next();
+        if (!isAuthPage)
+            return NextResponse.redirect(new URL('/login', request.url));
+        else
+            return NextResponse.next();
     }
 }
 
 export const config = {
-    matcher: ['/((?!login|signup|api|_next/static|_next/image|favicon.ico).*)']
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };

40607c926b00a18824e28d46d79f8b6970249fcf
Nishant <nishant.dahiya2000@gmail.com>
2025-07-30 21:19:00 +0530

    backend: add route for adding/updating s3 credentials

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,15 +1,58 @@
-import { fileUploadValidation } from '../types/user.schema.js';
+import { fileUploadValidation, updateS3CredentialsValidation } from '../types/user.schema.js';
 import { auth } from '../utils/auth.js';
+import { db } from '../db/drizzle.js';
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { getPutObjectSigned } from '../utils/s3.js';
+import { user } from '../db/schema.js';
+import { eq } from 'drizzle-orm';
 
 const userRoutes: FastifyPluginAsyncTypebox = async fastify => {
+    fastify.route({
+        method: 'PATCH',
+        url: '/update-s3-credentials',
+        schema: updateS3CredentialsValidation,
+        handler: async (request, reply) => {
+            const body = request.body;
+
+            // Authenticate user
+            const session = await auth.api.getSession({
+                headers: new Headers(Object.entries(request.headers) as [string, string][])
+            });
+            if (!session) return reply.status(401).send({ error: 'User not authenticated' });
+
+            // Update s3 credentials in db
+            const userId = session.user.id;
+
+            try {
+                const result = await db
+                    .update(user)
+                    .set({
+                        bucketName: body.bucketName,
+                        bucketRegion: body.bucketRegion,
+                        accessKey: body.accessKey,
+                        secretAccessKey: body.secretAccessKey,
+                        updatedAt: new Date()
+                    })
+                    .where(eq(user.id, userId))
+                    .returning();
+
+                if (result.length === 0) {
+                    return reply.status(404).send({ error: 'User not found or credentials could not be updated.' });
+                }
+
+                reply.status(200).send({ message: 'S3 credentials updated successfully', data: result[0] });
+            } catch (error) {
+                fastify.log.error('Error upserting S3 credentials:', error);
+                reply.status(500).send({ error: 'Failed to update/insert S3 credentials' });
+            }
+        }
+    });
+
     fastify.route({
         method: 'POST',
         url: '/file-upload',
         schema: fileUploadValidation,
         handler: async (request, reply) => {
-            // Get body and file
             const body = request.body;
 
             // Authenticate user
@@ -22,7 +65,7 @@ const userRoutes: FastifyPluginAsyncTypebox = async fastify => {
             const url = await getPutObjectSigned({ ...body });
             if (!url) return reply.status(500).send({ error: `Couldn't upload file: ${body.filename}` });
 
-            return reply.status(200).send({ data: url });
+            return reply.status(200).send({ message: 'Upload url created', data: url });
         }
     });
 };
--- backend/src/types/user.schema.ts
+++ backend/src/types/user.schema.ts
@@ -1,11 +1,21 @@
 import { Type } from '@fastify/type-provider-typebox';
 
+const S3Credentials = Type.Object({
+    accessKey: Type.String({ minLength: 1 }),
+    secretAccessKey: Type.String({ minLength: 1 }),
+    bucketName: Type.String({ minLength: 1 }),
+    bucketRegion: Type.String({ minLength: 1 })
+});
+
 export const fileUploadValidation = {
-    body: Type.Object({
-        accessKey: Type.String(),
-        secretAccessKey: Type.String(),
-        bucketName: Type.String(),
-        bucketRegion: Type.String(),
-        filename: Type.String()
-    }),
+    body: Type.Intersect([
+        S3Credentials,
+        Type.Object({
+            filename: Type.String({ minLength: 3 })
+        })
+    ])
+};
+
+export const updateS3CredentialsValidation = {
+    body: S3Credentials
 };

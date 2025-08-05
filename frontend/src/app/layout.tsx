d966acfb62e8152216d39a272f9856f1542d44f1
Nishant <nishant.dahiya2000@gmail.com>
2025-08-05 22:10:35 +0530

    frontend: apply inter font body wide
    font can be applied using classname but shadcn also requires font variables to be available for components

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Geist_Mono, Inter } from 'next/font/google';
 import './globals.css';
 
-const geistSans = Geist({
-    variable: '--font-geist-sans',
+const interSans = Inter({
+    variable: '--font-inter-sans',
     subsets: ['latin']
 });
 
@@ -24,7 +24,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+            <body className={`${interSans.className} ${interSans.variable} ${geistMono.variable} antialiased`}>
+                {children}
+            </body>
         </html>
     );
 }
